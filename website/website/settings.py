a72e9a0df4b86000475c400fa11ba5dfe61bd147
hayleypyle <hstowell@icloud.com>
2024-03-05 13:59:02 -0500

    changed debug to false and allowed hosts

--- website/website/settings.py
+++ website/website/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e*dxapv--6-#35b*lljzo^h6q=s0*7#xc(mrg7!+ml(b0qgcyl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://paper-trader-production-ff9b.up.railway.app"]
 
 
 # Application definition
